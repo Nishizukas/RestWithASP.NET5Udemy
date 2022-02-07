faa0292a3c9b3b781c6c60d6e6d2f53aa725cc92
Ricardo Nishizaki - RNI <ricardo.nishizaki@iteris.com.br>
2022-02-07 18:14:20 -0300

    Implementação de acesso ao banco + ajuste nas camadas do projeto

---
+++ 02_RestWithASPNETUdemy_Version/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,46 @@
+﻿using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private List<Person> persons = new List<Person>();
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository) 
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ 02_RestWithASPNETUdemy_Version/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Repository.Implementations;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNETUdemy", Version = "v1" });
+            });
+
+            //DB Connection String
+            string connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(
+                options => options.UseMySql(
+                    connection, 
+                    ServerVersion.AutoDetect(connection)
+                )
+            );
+
+            //Versionamento
+            services.AddApiVersioning();
+
+            //Injeção de Dependencia
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestWithASPNETUdemy v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
